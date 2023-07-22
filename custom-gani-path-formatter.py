65913bb827522ccb0c143345907e0b8a604b9e02
aminimal <aminimal@pm.me>
2023-07-22 23:53:24 +0100

    gui, filename prefixes, output folder
    User's can select file to search using gui as well as where to put generated files. Added sophistication around gani filename prefixes generated by MtarTool.

--- custom-gani-path-formatter.py
+++ custom-gani-path-formatter.py
@@ -1,30 +1,70 @@
+from sys import exit
 import pathlib
+from tkinter import filedialog
+from tkinter import *
 
-# store path objects for gani files in list
-path = pathlib.Path('Assets')
-gen = path.rglob('*.gani')
-list = list(gen)
+def exitOnEmptyVariable(x):
+    if not x:
+        exit()
 
-# lists of lines for each type
-gani_stems = []
-gani_lines = []
-gani_lines_xml = []
-gani_lines_lua_motions = []
-gani_lines_lua_motiongroups = []
+# get input directory from user
+selected_folder = filedialog.askdirectory(initialdir='/', title='Select the folder created by MtarTool (e.g. "custom_motions_mtar")')
+exitOnEmptyVariable(selected_folder)
+input_dir = pathlib.Path(selected_folder)
+input_dir_name = input_dir.stem
 
-# iterate through each gani file and generate lines
-for i, item in enumerate(list):
-    itemid = "CGPF" + f"{i:04}"
-    posix = item.as_posix()
-    stem = pathlib.Path(item).stem.split('.')[0]
-    gani_lines.append('/' + posix[:-5])
-    gani_lines_xml.append('<Gani FilePath=\"' + '/' + posix[:-5] + '\" />')
-    gani_lines_lua_motions.append(itemid + '={\"' + '/' + posix + '\"},')
-    gani_lines_lua_motiongroups.append('\"' + itemid + '\",')
+# get output directory from user
+selected_folder = filedialog.askdirectory(initialdir='/', title='Select the folder for storing the output files')
+exitOnEmptyVariable(selected_folder)
+output_dir = pathlib.Path(selected_folder)
+output_dir_name = output_dir.stem
 
-# generate files for each type
-with open(f'{gani_lines=}'.split('=')[0] + '.txt', 'w') as file:
-    lines = gani_lines
+# create output file paths
+of1_path = pathlib.PurePath.joinpath(output_dir, input_dir_name + '_mtar_dictionary.txt')
+of2_path = pathlib.PurePath.joinpath(output_dir, input_dir_name + '_xml.txt')
+of3_path = pathlib.PurePath.joinpath(output_dir, input_dir_name + '_lua_motion.txt')
+
+# get list of gani from input_dir as root
+gparts = []
+for g in input_dir.rglob('*.gani'):
+        leading_characters = g.stem[:4]
+        char5 = g.stem[4]
+        if leading_characters.isnumeric and char5 == '_':
+            xtuple = g.relative_to(input_dir).parts
+            xlist = list(xtuple)
+            xlist[-1] = xlist[-1][5:]
+            ytuple = tuple(xlist)
+            gparts.append(ytuple)
+        else:
+            gparts.append(g.relative_to(input_dir).parts)
+exitOnEmptyVariable(gparts)
+
+# generate generic path list
+paths_invalid = []
+plist = []
+for g in gparts:
+    leng = len(g)
+    if leng < 3:
+        paths_invalid.append('/' + '/'.join(g))
+    else:
+        plist.append('/' + '/'.join(g))
+
+# generate lines for mtar dictionary and mtar .xml
+paths_mtar_dict = []
+paths_mtar_xml = []
+for p in plist:
+    paths_mtar_dict.append(p.replace('.gani', ''))
+    paths_mtar_xml.append('<Gani FilePath=\"' + p.replace('.gani', '') + '\" />')
+
+# generate lines for lua
+paths_lua_motions = []
+for i, p in enumerate(plist):
+    mgid = "MOTION" + f"{i:04}"
+    paths_lua_motions.append(mgid + '={\"' + str(p) + '\"},')
+
+# generate files
+with open(of1_path, 'w') as file:
+    lines = paths_mtar_dict
     cnt = len(lines)
     for i, line in enumerate(lines):
         if not i == cnt - 1:
@@ -32,8 +72,8 @@ with open(f'{gani_lines=}'.split('=')[0] + '.txt', 'w') as file:
         else:
             file.write(line)
 
-with open(f'{gani_lines_xml=}'.split('=')[0] + '.txt', 'w') as file:
-    lines = gani_lines_xml
+with open(of2_path, 'w') as file:
+    lines = paths_mtar_xml
     cnt = len(lines)
     for i, line in enumerate(lines):
         if not i == cnt - 1:
@@ -41,22 +81,11 @@ with open(f'{gani_lines_xml=}'.split('=')[0] + '.txt', 'w') as file:
         else:
             file.write(line)
 
-with open(f'{gani_lines_lua_motions=}'.split('=')[0] + '.txt', 'w') as file:
-    lines = gani_lines_lua_motions
+with open(of3_path, 'w') as file:
+    lines = paths_lua_motions
     cnt = len(lines)
     for i, line in enumerate(lines):
         if not i == cnt - 1:
             file.write(line + '\n')
         else:
             file.write(line)
-
-with open(f'{gani_lines_lua_motiongroups=}'.split('=')[0] + '.txt', 'w') as file:
-    lines = gani_lines_lua_motiongroups
-    cnt = len(lines)
-    for i, line in enumerate(lines):
-        if not i == cnt - 1:
-            file.write(line + '\n')
-        else:
-            file.write(line)
-
-# end
